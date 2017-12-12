94a142cb2240f8c050abbeebe93ae5f41ab186fc
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-12-12 01:37:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(int)
   if int % 3 == 0 &&  int % 5 == 0
     "FizzBuzz"
-#  elseif int % 3 == 0
-#    "Fizz"
+  elseif int % 3 == 0
+    "Fizz"
 #  elseif int % 5 == 0
 #    "buzz"
 #  else
